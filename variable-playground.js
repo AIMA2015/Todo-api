9fa52a87817164da03fbb38f1db8b1e9032bedde
Ana Medina Ayala <xesquiz@gmail.com>
2015-12-02 18:53:24 -0500

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,21 @@
+// var person = {
+// 	name: 'Ana',
+// 	age: 21
+// };
+
+// function updatePerson(obj) {
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+var grades = [56, 67];
+
+function updateGrades(obj) {
+	obj[1] = 90;
+}
+
+updateGrades(grades);
+console.log(grades);
